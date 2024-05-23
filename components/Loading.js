a6ec5f98e6dbfe3795d678f02b1b1090a9c9901a
Sonam Jamtsho <sjamtsho@myseneca.ca>
2024-05-23 12:50:57 -0400

    updated style property

--- components/Loading.js
+++ components/Loading.js
@@ -1,9 +1,12 @@
-import { Spinner } from "react-bootstrap";
+import { Spinner, Container } from "react-bootstrap";
 import React from 'react'
 
 export default function Loading() {
     return (
-        <Spinner animation="border" role="status" style={{position:'absolute', top: '50%', left:'50%'}}>
-            <span className="visually-hidden">Loading...</span>
-        </Spinner>)
+        <Container className="w-100" style={{position:'relative', height:'100vh'}}>
+            <Spinner animation="border" role="status" style={{ position:'absolute', top: '50%', left: '50%' }}>
+                <span className="visually-hidden">Loading...</span>
+            </Spinner>
+        </Container>
+    )
 }
